3975b9c826b34c41454cdb294832479333b6c71b
hecgzz <hectorgzzgaona@gmail.com>
2021-03-26 23:54:08 -0600

    Updating Database

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-regular-67187',
     user : 'postgres',
     password : 'epyrus',
     database : 'face-recognition'
